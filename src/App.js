34e283265ff40a044a3f5e31bea07e6a06d5d04f
B-Litty <bradlitman94@gmail.com>
2021-05-03 11:14:00 -0700

    First Commit for Pic of the Day

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
+import {Card} from 'react-bootstrap';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [picture, setPicture] = useState(null);
+  const [picInfo, setPicInfo] = useState(null);
+  
+
+  useEffect(() => {
+    async function fetchData() {
+    const response = await fetch(
+      `https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_API_KEY}`
+    );
+    const data = await response.json();
+    setPicture(data.url);
+    setPicInfo(data);
+  }
+  fetchData();
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="body">
+      <div className="title">
+        <h1>Nasa`s Picture of the day</h1>
+      </div>
+          <Card style={{ width: '35rem' }}>
+            <Card.Img variant="top" src={picture} />
+            <Card.Body>
+              {picInfo && <Card.Title>{picInfo.title}</Card.Title>}
+              {picInfo && <Card.Text>{picInfo.explanation}</Card.Text>}
+            </Card.Body>
+            <Card.Body>
+              {picInfo && <Card.Link href={picture}>Link to original photo</Card.Link>}
+            </Card.Body>
+            {picInfo && <Card.Footer className="text-muted">Date: {picInfo.date}</Card.Footer>}
+          </Card>
+     </div>
   );
 }
+
 
 export default App;
